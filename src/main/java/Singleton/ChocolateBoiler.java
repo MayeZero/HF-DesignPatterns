a1ea896e0f1ceffc8b5a7a74d964db7867570551
Zero <997398541@qq.com>
2020-09-15 18:58:26 +0800

    add Singleton ChocolateBoiler

---
+++ src/main/java/Singleton/ChocolateBoiler.java
@@ -0,0 +1,59 @@
+package Singleton;
+
+public class ChocolateBoiler {
+    private boolean empty;
+    private boolean boiled;
+
+    private static ChocolateBoiler singleChocolateBoiler;
+
+    private ChocolateBoiler(){
+        empty = true;
+        boiled = false;
+    }
+
+    public static ChocolateBoiler getInstance(){
+        if(singleChocolateBoiler == null){
+            singleChocolateBoiler = new ChocolateBoiler();
+        }
+        return singleChocolateBoiler;
+    }
+
+    public void fill(){
+        if(isEmpty()){
+            empty = false;
+            boiled = false;
+            System.out.println("ChocolateBoiler is filled!");
+        }
+    }
+
+    public void drain(){
+        if(!isEmpty() && isBoiled()){
+            empty = true;
+            System.out.println("ChocolateBoiler is drained");
+        }
+    }
+
+    public void boil(){
+        if(!isEmpty() && !isBoiled()){
+            boiled = true;
+            System.out.println("ChocolateBoiler is boiled");
+        }
+    }
+
+    public boolean isEmpty(){
+        return empty;
+    }
+
+    public boolean isBoiled(){
+        return boiled;
+    }
+}
+
+class ChocolateBoilerTest{
+    public static void main(String[] args) {
+        ChocolateBoiler chocolateBoiler = ChocolateBoiler.getInstance();
+        chocolateBoiler.fill();
+        chocolateBoiler.boil();
+        chocolateBoiler.drain();
+    }
+}
