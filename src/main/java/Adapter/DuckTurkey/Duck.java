e394e562652e674a3b97bde57c0fbfe505c18ed1
Zero <997398541@qq.com>
2020-09-20 16:45:30 +0800

    add adapter pattern

---
+++ src/main/java/Adapter/DuckTurkey/Duck.java
@@ -0,0 +1,7 @@
+package Adapter.DuckTurkey;
+
+public interface Duck {
+    public void quack();
+
+    public void fly();
+}
---
+++ src/main/java/Adapter/DuckTurkey/Turkey.java
@@ -0,0 +1,7 @@
+package Adapter.DuckTurkey;
+
+public interface Turkey {
+    public void gobble();
+
+    public void fly();
+}
